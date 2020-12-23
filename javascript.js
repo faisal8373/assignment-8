b20f99d0fb68004691c119fa573d2922482d0d7a
faisal8373 <59798752+faisal8373@users.noreply.github.com>
2020-12-24 00:34:30 +0500

    third program

--- javascript.js
+++ javascript.js
@@ -8,3 +8,13 @@ if(height){
     var cm = height * 2.54
     alert('Height in centimeter is: ' + cm)
     }
+    
+var eng = prompt('Please enter marks in English: ')
+var math = prompt('Please enter marks in math: ')
+var computer = prompt('Please enter marks in computer: ')
+if(eng & math & computer){
+var avg = (eng + math + computer)/3
+if (avg >= 80) {
+    alert("you are above standard, Admission Granted!")
+}
+}
