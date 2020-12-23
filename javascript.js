8e54fb80b15db92e37dca68199e9e988446eb9d4
faisal8373 <59798752+faisal8373@users.noreply.github.com>
2020-12-24 00:33:38 +0500

    second program

--- javascript.js
+++ javascript.js
@@ -3,3 +3,8 @@ if (temp){
 var forn = 9/5 * temp + 32
 alert('Temperatur in foren is: ' + forn)
 }
+if(height){
+    var height = prompt('Please enter height in inches: ')
+    var cm = height * 2.54
+    alert('Height in centimeter is: ' + cm)
+    }
